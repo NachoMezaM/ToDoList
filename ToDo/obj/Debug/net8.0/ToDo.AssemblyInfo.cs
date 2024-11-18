681248f2d22284c7feed5ef057c467c237d7c01e
NachoMezaM <nachom1605@gmail.com>
2024-11-18 18:54:51 -0300

    Commit Inicial

--- ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8bec31c67d25b5b513841cae3d12f689a93fc25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f18c9855448cd2e2ffd3e1fd3717c4e1f5fdcbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
